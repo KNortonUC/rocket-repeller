715367ebd1f6736baa7948227ba7503b42dd879c
Kaleb Norton <nortonkb@mail.uc.edu>
2025-04-21 19:11:16 -0400

    Added Game Class and Window Creation

---
+++ js/game.js
@@ -0,0 +1,25 @@
+var config = 
+{
+	// Screen 
+	width: 924,
+	height: 800,
+	backgroundColor: 0x000000,
+	
+	// Scenes Preload
+	//scene: [Scene1, Scene2],
+	
+	// Physics 
+	physics: 
+	{
+		default: "arcade",
+		arcade:{
+			debug: false
+		}
+	}
+}
+
+// Start Game Window //
+window.onload = function() 
+{
+	var game = new Phaser.Game(config);
+}
